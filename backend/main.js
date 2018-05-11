39a40c894996e2e01ad9d94209e36221a48be235
Vladislav <vladislav.paskar@uni-ulm.de>
2018-05-11 11:24:36 +0200

    eslint fixes

--- backend/main.js
+++ backend/main.js
@@ -1,25 +1,21 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-const path = require('path');
-var http = require('http');
-var fs = require('fs');
-
+const path = require("path");
 
 //load view engine
-app.use(express.static(path.join(__dirname, '../frontend/views')));
-// app.engine('html', engines.mustache);
- 
-app.get('/', function (req, res) {
-    res.send('edit.html');
-})
+app.use(express.static(path.join(__dirname, "../frontend/views")));
+// app.engine("html", engines.mustache);
+
+app.get("/", function (req, res) {
+	res.send("edit.html");
+});
 
 const port = process.argv[2];
 
-
 //start server
-app.listen(port, function(){
-    console.log('Server started on port '+port);
+app.listen(port, function () {
+	console.log("Server started on port " + port);
 });
-
 //bowserify TODO
-// module.exports = 
+// module.exports =
+
