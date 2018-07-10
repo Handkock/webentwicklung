2e8d1f60501f96a3b06b0240d43d6881bd493ffd
Vladislav <vladislav.paskar@uni-ulm.de>
2018-07-10 23:00:13 +0200

    big commit

--- backend/main.js
+++ backend/main.js
@@ -1,31 +1,131 @@
 var express = require("express");
 var app = express();
 const path = require("path");
-const Session = require(path.join(__dirname, "models/Session"));
+const mongoose = require("mongoose");
+const config = require("./config/database");
 
-const dbClient = require("mongodb").MongoClient;
-dbClient.connect("mongodb://localhost:27017/webDev", (error, db) => {
-	if (error) {
-		console.error(error);
-		process.exit(-1);
-	}
-	console.log("Connected to MongoDB.");
-	try {
-		console.log("do smh..");
-	}
-	finally {
-		db.close();
-	}
+//init db
+mongoose.connect(config.database);
+let db = mongoose.connection;
+
+//check db connection
+db.once("open", function () {
+	console.log("Connected to mongodb");
 });
+
+//check for db errors
+db.on("error", function (err) {
+	console.log(err);
+});
+app.set('views', path.join(__dirname, '.'));
+app.set('view engine', 'pug');
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+//include Session model
+const Session = require("./models/session");
 
 //load view engine
 app.use(express.static(path.join(__dirname, "..")));
 // app.engine('html', engines.mustache);
 
+
 app.get("/", function (req, res) {
-	res.sendFile(path.join(__dirname, "../main.html"));
-	let ses1 = new Session(120, 101, new Date(), ["1", "23"]);
+	Session.find({}, function (err, sessions) {
+		if (err) {
+			console.log(err);
+		}
+		else {
+			res.render("main", {
+				sessions: sessions
+			});
+		}
+	});
 });
+
+app.get("/newSession", function (req, res) {
+	res.render("main", {
+		newSession: true
+	});
+});
+
+app.post("/newSession", function (req, res) {
+
+	let session = new Session();
+	session.latitude = req.body.latitude;
+	session.longtitude = req.body.longtitude;
+	session.date = req.body.date;
+	session.objects = "asd";
+	session.save(function (err) {
+		if (err) {
+			console.log(err);
+			return;
+		}
+		else {
+			res.redirect("/");
+		}
+	});
+});
+
+app.get("/editSession/:id", function (req, res) {
+	Session.findById(req.params.id, function (err, session) {
+		var date = new Date(session.date);
+		let month = "" + (date.getMonth() + 1);
+		let day = "" + date.getDate();
+		let year = date.getFullYear();
+		if (month.length < 2) {
+			month = "0" + month;
+		}
+		if (day.length < 2) {
+			day = "0" + day;
+		}
+		date = year + "-" + month + "-" + day;
+		res.render("main", {
+			editSession: true,
+			session: session,
+			date: date,
+		});
+	});
+});
+
+app.post("/editSession/:id", function (req, res) {
+	Session.findById(req.params.id, function (err, session) {
+		console.log(req.params.id);
+		session.latitude = req.body.latitude;
+		session.longtitude = req.body.longtitude;
+		session.date = req.body.date;
+		session.objects = "ss";
+		session.save(function (err) {
+			if (err) {
+				console.log(err);
+				return;
+			}
+			else {
+				res.redirect("/");
+			}
+		});
+	});
+});
+
+app.delete("/deleteSession/:id", function (req, res) {
+	let query = { id: req.params._id };
+
+	Session.findById(req.params._id, function (err, session) {
+		Session.remove(query, function (err) {
+			if (err) {
+				console.log(err);
+			}
+			else {
+				res.redirect("/");
+			}
+		});
+	});
+});
+
 const port = process.argv[2];
 
 //start server
--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,1 +1,15 @@
-console.log("s");
+$(document).ready(function(){
+	$(".delete-form").submit( function(eventObj) {
+			const id = $(".delete-session").attr("data-id");
+			$.ajax({
+				type: 'DELETE',
+				url: '/deleteSession/' + id,
+				success: function (response){					
+					return false;
+				},
+				error: function(err){
+				  console.error(err);
+				}
+			});
+	});
+});
